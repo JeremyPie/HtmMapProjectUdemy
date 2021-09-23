7c93d9a060c88a91d949ccb0261d338d028598ed
JeremyPie <91102181+JeremyPie@users.noreply.github.com>
2021-09-23 03:39:44 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import folium
 import pandas as pd
 
-
 df = pd.read_csv("Volcanoes.txt")
 
 html = """<h4>Volcanoe Information:</h4>
@@ -16,12 +15,20 @@ def color_generator(elevation):
     else: return 'black'
 
 map = folium.Map(location=[30.75624245178316, 34.88149453543761], zoom_start=9, tiles = "Stamen Terrain")
-fg1 = folium.FeatureGroup(name="My Map")
+fg1 = folium.FeatureGroup(name="Volcanoes")
+fg2 = folium.FeatureGroup(name="Population")
+
 
 for name, elev, lat, lon in zip(df['NAME'], df['ELEV'], df['LAT'], df['LON']):
     iframe = folium.IFrame(html=html.format(name,name,elev), width=200, height=100)
     fg1.add_child(folium.CircleMarker(location=[lat, lon], popup=folium.Popup(iframe), radius=6,
     color=color_generator(elev), fill=True, fill_color=color_generator(elev), opacity=0.7, fill_opacity=0.7))
-    map.add_child(fg1)
+
+fg2.add_child(folium.GeoJson(data=open('world.json', encoding='utf-8-sig').read(),
+                             style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] <= 10000000
+                                                       else 'orange' if 10000000 < x['properties']['POP2005'] < 20000000 else 'red'}))
+map.add_child(fg1)
+map.add_child(fg2)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
